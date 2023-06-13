ec2673084126715fcd44c6d90c1038ef86248048
mrkotbest <mrkotbest@gmail.com>
2023-06-13 21:27:10 +0300

    entity framework core tools installed + created DbContext & ReservationDTO (Data Transfer Object)

---
+++ WPF_Reservation/DTOs/ReservationDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Reservation.DTOs
+{
+    public class ReservationDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int FloorNumber { get; set; }
+        public int RoomNumber { get; set; }
+        public string Username { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ WPF_Reservation/DbContexts/ReservationRoomDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Reservation.DTOs;
+using WPF_Reservation.Models;
+
+namespace WPF_Reservation.DbContexts
+{
+    public class ReservationRoomDbContext : DbContext
+    {
+        public DbSet<ReservationDTO> Reservations { get; set; }
+    }
+}
